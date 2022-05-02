85df9e06ca9da9732beaa3df558f168abd87faf5
aniket-sk <aniketsk2001@gmail.com>
2022-05-03 03:56:40 +0530

    first commit

---
+++ js/news.js
@@ -0,0 +1,53 @@
+console.log("This is my index js file");
+
+// Initialize the news api parameters
+let category = "business";
+let domains = "finance.yahoo.com";
+let sortby = "relevancy";
+let language = "en";
+let pagesize = "5";
+let apiKey = "c729e26ec97e4b6b905df8a6c90f38da";
+
+// Grab the news container
+let newsAccordion = document.getElementById("newsAccordion");
+
+// Create an ajax get request
+const xhr = new XMLHttpRequest();
+xhr.open(
+  "GET",
+  `https://newsapi.org/v2/top-headlines?q=stock&category=${category}&domains=${domains}&sortby=${sortby}&language=${language}&pagesize=${pagesize}&apiKey=${apiKey}`,
+  true
+);
+
+// What to do when response is ready
+xhr.onload = function () {
+  if (this.status === 200) {
+    let json = JSON.parse(this.responseText);
+    let articles = json.articles;
+    console.log(articles);
+    let newsHtml = "";
+    articles.forEach(function (element, index) {
+      // console.log(element, index)
+      let news = `<div class="card">
+                            <div class="card-header" id="heading${index}">
+                                <h2 class="mb-0">
+                                <button class="btn btn-light collapsed" type="button" data-toggle="collapse" data-target="#collapse${index}"
+                                    aria-expanded="false" aria-controls="collapse${index}">
+                                   • ${element["title"]}
+                                </button>
+                                </h2>
+                            </div>
+
+                            <div id="collapse${index}" class="collapse" aria-labelledby="heading${index}" data-parent="#newsAccordion">
+                                <div class="card-body"> ${element["content"]}. <a href="${element["url"]}" target="_blank" >Read more</a>  </div>
+                            </div>
+                        </div>`;
+      newsHtml += news;
+    });
+    newsAccordion.innerHTML = newsHtml;
+  } else {
+    console.log("Some error occured");
+  }
+};
+
+xhr.send();
---
+++ js/signUp.js
@@ -0,0 +1,45 @@
+(function($) {
+
+	"use strict";
+
+	$(".toggle-password").click(function() {
+
+  $(this).toggleClass("fa-eye fa-eye-slash");
+  var input = $($(this).attr("toggle"));
+  if (input.attr("type") == "password") {
+    input.attr("type", "text");
+  } else {
+    input.attr("type", "password");
+  }
+});
+
+})(jQuery);
+
+const username=document.getElementById("username-field");
+const password=document.getElementById("password-field");
+const signUp=document.getElementById("signup-btn");
+signUp.addEventListener('click',e=>{
+  e.preventDefault();
+  const usernameValue=username.value;
+  const passwordValue=password.value;
+  const auth=firebase.auth();
+  
+  console.log(usernameValue);
+  console.log(passwordValue);
+  if(usernameValue==""){
+      alert("Please enter your username");
+  }
+  else if(passwordValue==""){
+      alert("Please enter your password");
+  }
+  else{
+    auth.createUserWithEmailAndPassword(usernameValue, passwordValue).then(user =>{
+      alert('Succesfully signed up');
+  }).catch(err => {
+      alert(err.message);
+  });
+    
+    
+  }
+
+});
